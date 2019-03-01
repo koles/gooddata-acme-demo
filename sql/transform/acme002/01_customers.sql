78f1c86257229d368225a6518e91382a07c474aa
corourke <cameron.orourke@gmail.com>
2019-02-28 23:02:40 -0800

    Model and SQL xform for tut 002

---
+++ sql/transform/acme002/01_customers.sql
@@ -0,0 +1,20 @@
+TRUNCATE TABLE out_customers;
+INSERT INTO out_customers (
+	a__age,
+	a__gender,
+	a__email_address,
+	a__city,
+	a__state,
+	a__postal_code,
+	a__country_code,
+	cp__id )
+SELECT 
+	AGE_IN_YEARS(TO_DATE(birthdate, 'YYYY-MM-DD')),
+	gender,
+	email_address,
+	city, 
+	state,
+	postal_code,
+	country_code2,
+	TO_NUMBER(customer_id)
+FROM stg_csv_customers_merge;
